26506e35f8b72eb7e338132cd6e6e9f98e4b0b2b
Kacper Mularczyk <165844572+kacpermularczyk@users.noreply.github.com>
2024-04-15 03:25:19 +0200

    Add files via upload

---
+++ php_06a_nowastruktura/app/controllers/CalcCtrl.class.php
@@ -0,0 +1,95 @@
+<?php
+
+require_once 'CalcForm.class.php';
+require_once 'CalcResult.class.php';
+
+class CalcCtrl
+{ 
+	private $form;   
+	private $res;
+	
+	private $interest;
+	private $semiResult;
+	private $months;
+
+	public function __construct()
+	{
+		$this->form = new CalcForm();
+		$this->res = new CalcResult();
+	}
+	public function getParams()
+	{
+		$this->form->amount = getFromRequest('amount');
+		$this->form->years = getFromRequest('years');
+		$this->form->interestRate = getFromRequest('interestRate');
+	}
+	public function validate()
+	{
+		if (! (isset($this->form->amount) && isset($this->form->years) && isset ($this->form->interestRate))) return false;
+		
+		if ($this->form->amount == "")
+		{
+			getMessages()->addError('Nie podano kwoty');
+		}
+		if ($this->form->years == "")
+		{
+			getMessages()->addError('Nie podano na ile lat');
+		}
+		if ($this->form->interestRate == "")
+		{
+			getMessages()->addError('Nie podano oprocentowania');
+		}
+		
+		if (! getMessages()->isError())
+		{
+			if (! is_numeric($this->form->amount))
+			{
+				getMessages()->addError('Pierwsza wartość nie jest liczbą całkowitą');
+			}
+			if (! is_numeric($this->form->years))
+			{
+				getMessages()->addError('Druga wartość nie jest liczbą całkowitą');
+			}
+			if (! is_numeric($this->form->interestRate))
+			{
+				getMessages()->addError('Trzecia wartość nie jest liczbą całkowitą');
+			}
+		}
+		
+		return ! getMessages()->isError();
+	}
+	public function process()
+	{
+		$this->getparams();
+		
+		if ($this->validate())
+		{
+			$this->form->amount = intval($this->form->amount);
+			$this->form->years = intval($this->form->years);
+			$this->form->interestRate = intval($this->form->interestRate);
+			getMessages()->addInfo('Parametry poprawne.');
+				
+			$this->interest = $this->form->amount * $this->form->years * $this->form->interestRate * 0.01;
+			$this->semiResult = $this->form->amount + $this->interest;
+			$this->months = $this->form->years * 12;
+			$this->res->result = number_format(($this->semiResult/$this->months),2, ',', ''); // 1 - wartosc, 2 - po przecinku, 3 - separator dziesiatek 4 - sep. tysiecy
+			
+			getMessages()->addInfo('Wykonano obliczenia.');
+		}
+		
+		$this->generateView();
+	}
+	public function generateView()
+	{
+		
+		getSmarty()->assign('page_title','KacperMularczykNowaStruktura');
+		getSmarty()->assign('page_description','kalkulator kredytowy');
+		getSmarty()->assign('first_part_of_title','kalkulator');
+		getSmarty()->assign('second_part_of_title','kredytowy');
+		
+		getSmarty()->assign('form',$this->form);
+		getSmarty()->assign('res',$this->res);
+		
+		getSmarty()->display('calcView.html');
+	}
+}
---
+++ php_06a_nowastruktura/core/Messages.class.php
@@ -0,0 +1,44 @@
+<?php
+class Messages
+{
+	private $errors = array();
+	private $infos = array();
+	private $counter = 0;
+
+	public function addError($message)
+	{
+		$this->errors[] = $message;
+		$this->counter++;
+	}
+	public function addInfo($message)
+	{
+		$this->infos[] = $message;
+		$this->counter++;
+	}
+	public function isEmpty()
+	{
+		return $this->counter == 0;
+	}
+	public function isError()
+	{
+		return count($this->errors) > 0;
+	}
+	public function isInfo()
+	{
+		return count($this->infos) > 0;
+	}
+	public function getErrors()
+	{
+		return $this->errors;
+	}
+	public function getInfos()
+	{
+		return $this->infos;
+	}
+	public function clear()
+	{
+		$this->errors = array();
+		$this->infos = array();
+		$this->counter = 0;
+	}
+}
